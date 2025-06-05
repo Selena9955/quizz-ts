115b72541a121a0d0e90735a681cb70aa667652f
min <jorg77jorg77@gmail.com>
2025-06-05 17:02:23 +0800

    新增 articles 標題欄位、調整 quill 邊框樣式

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,26 +1,31 @@
 import React from "react";
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
+import { cn } from "@/lib/utils";
 
 function InputField({
   label,
   description,
   message,
   messageColor = "text-danger",
+  labelClassName,
+  inputClassName,
   ...rest
 }: {
   label: string;
   description?: string;
   message?: string;
   messageColor?: string;
+  labelClassName?: string;
+  inputClassName?: string;
 } & React.InputHTMLAttributes<HTMLInputElement>) {
   return (
     <div className="space-y-2">
-      <Label className="font-bold">{label}</Label>
+      <Label className={cn("font-bold", labelClassName)}>{label}</Label>
       {description && (
         <p className="text-muted-foreground text-xs">{description}</p>
       )}
-      <Input {...rest} />
+      <Input {...rest} className={inputClassName} />
       {message && <p className={`${messageColor} text-xs`}>{message}</p>}
     </div>
   );
--- src/pages/ArticleNew.tsx
+++ src/pages/ArticleNew.tsx
@@ -2,8 +2,11 @@ import "quill/dist/quill.snow.css";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import QuillEditor from "@/components/QuillEditor";
+import InputField from "@/components/InputField";
+import { Label } from "@/components/ui/label";
 
 function ArticleNew() {
+  const [title, setTitle] = useState<string>("");
   const [editorValue, setEditorValue] = useState<string>("");
 
   function handleEditorChange(newValue: string) {
@@ -12,10 +15,21 @@ function ArticleNew() {
 
   return (
     <div>
+      <InputField
+        label="標題"
+        labelClassName=" text-base  md:text-lg  font-semibold"
+        placeholder="請輸入標題"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+
+      <Label className="mt-6 mb-2 text-base font-semibold md:text-lg">
+        內文
+      </Label>
       <QuillEditor value={editorValue} onChange={handleEditorChange} />
 
       <div className="mt-6 flex justify-end">
-        <Button>Add</Button>
+        <Button size="lg">新增文章</Button>
       </div>
       <p>{editorValue}</p>
     </div>
