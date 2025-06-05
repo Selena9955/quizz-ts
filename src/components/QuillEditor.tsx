d5a9ca06ebd4280776527b7a1b737e201e390a38
min <jorg77jorg77@gmail.com>
2025-06-05 14:47:48 +0800

    引入quill套件，建立基本文字編輯器

---
+++ src/components/QuillEditor.tsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useRef, useState } from "react";
+import Quill from "quill";
+
+type QuillEditorProps = {
+  value?: string;
+  onChange?: (value: string) => any;
+};
+const QuillEditor = ({ value, onChange }: QuillEditorProps): JSX.Element => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const lastChangeRef = React.useRef<string | null>(null);
+
+  const [quill, setQuill] = useState<Quill | null>(null);
+
+  useEffect(() => {
+    if (!quill) {
+      return;
+    }
+
+    if (value !== lastChangeRef.current) {
+      quill.root.innerHTML = value || "";
+    }
+  }, [quill, value]);
+
+  useEffect(() => {
+    if (!quill) {
+      return;
+    }
+
+    function handleChange() {
+      const contents = quill!.root.innerHTML;
+      lastChangeRef.current = contents;
+
+      console.log("HERE!");
+
+      if (onChange) {
+        onChange(contents);
+      }
+    }
+
+    quill.on("text-change", handleChange);
+
+    return () => {
+      quill.off("text-change", handleChange);
+    };
+  }, [quill, onChange]);
+
+  useEffect(() => {
+    const container = containerRef.current;
+    if (!container) {
+      return;
+    }
+
+    const editorElement = document.createElement("div");
+    container.append(editorElement);
+    let quill: Quill = new Quill(editorElement, {
+      theme: "snow",
+    });
+
+    quill.root.innerHTML = value || "";
+
+    setQuill(quill);
+
+    return () => {
+      container.innerHTML = "";
+    };
+  }, []);
+
+  return <div ref={containerRef}></div>;
+};
+
+export default QuillEditor;
--- src/pages/ArticleNew.tsx
+++ src/pages/ArticleNew.tsx
@@ -1,5 +1,25 @@
+import "quill/dist/quill.snow.css";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import QuillEditor from "@/components/QuillEditor";
+
 function ArticleNew() {
-  return <div>ArticleAdd</div>;
+  const [editorValue, setEditorValue] = useState<string>("");
+
+  function handleEditorChange(newValue: string) {
+    setEditorValue(newValue);
+  }
+
+  return (
+    <div>
+      <QuillEditor value={editorValue} onChange={handleEditorChange} />
+
+      <div className="mt-6 flex justify-end">
+        <Button>Add</Button>
+      </div>
+      <p>{editorValue}</p>
+    </div>
+  );
 }
 
 export default ArticleNew;
